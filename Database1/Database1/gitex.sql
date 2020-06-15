79b4b44932e9ad99abe2b0e96144d677bad80692
jyo <jyothi.telkar@gmail.com>
2020-06-15 14:17:31 -0700

    added new parameters to stored procedure

--- Database1/Database1/gitex.sql
+++ Database1/Database1/gitex.sql
@@ -1,6 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[gitex]
 	@ID int = 0,
-	@companyname  nvarchar(256)
+	@companyname  nvarchar(256),
+	@comapnyaddress nvarchar(250),
+	@city nvarchar(100)
 AS
     insert into gitexample.CustomersSSIS values(@ID,@companyname)
 	SELECT @ID, @companyname
