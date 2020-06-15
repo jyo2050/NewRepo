f163fd39a323577ed3d566ebb1b7903da2f19c6d
jyo <jyothi.telkar@gmail.com>
2020-06-15 14:39:40 -0700

    correct spelling istake in stored procedure parameter

--- Database1/Database1/gitex.sql
+++ Database1/Database1/gitex.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[gitex]
 	@ID int = 0,
 	@companyname  nvarchar(256),
-	@comapnyaddress nvarchar(250),
+	@companyaddress nvarchar(250),
 	@city nvarchar(100)
 AS
     insert into gitexample.CustomersSSIS values(@ID,@companyname,@companyaddress,@city)
