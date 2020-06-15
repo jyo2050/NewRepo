54846dae9b97cb45ca960e4a45f774d077d59257
jyo <jyothi.telkar@gmail.com>
2020-06-15 14:15:20 -0700

    stored procedure commit

---
+++ Database1/Database1/gitex.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[gitex]
+	@ID int = 0,
+	@companyname  nvarchar(256)
+AS
+    insert into gitexample.CustomersSSIS values(@ID,@companyname)
+	SELECT @ID, @companyname
+RETURN 0
